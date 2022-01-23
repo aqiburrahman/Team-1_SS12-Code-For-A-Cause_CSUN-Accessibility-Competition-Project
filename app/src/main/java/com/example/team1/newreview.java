e20a5075f9601d0b5edde81f2fe2a6be6f88ad14
Unavailable <aqibur.rahman.717@my.csun.edu>
2022-01-23 10:15:14 -0800

    testing writing file

--- app/src/main/java/com/example/team1/newreview.java
+++ app/src/main/java/com/example/team1/newreview.java
@@ -3,21 +3,40 @@ package com.example.team1;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class newreview extends AppCompatActivity {
+    File file;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newreview);
+        File d=getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        file = new File(d,"data.txt");
+
     }
-    public void takecomment(View v){
+    public void takecomment(View v) throws IOException {
         TextView t = findViewById(R.id.enterText);
         String input = t.getText().toString();
         ((TextView)findViewById(R.id.output)).setText(input);
+
+        FileWriter writer=new FileWriter(file);
+        EditText editText=(EditText)findViewById(R.id.enterText);
+        writer.append(editText.getText().toString());
+        writer.close();
+
+        readData(findViewById(R.id.output));
     }
     public void disableGreen(View v){
 
@@ -39,5 +58,20 @@ public class newreview extends AppCompatActivity {
     public void uploaderClick(View v) {
         Intent i = new Intent(this, uploader.class);
         startActivity(i);
+    }
+
+
+    public void readData(View view) throws IOException {
+        File d=getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        File file=new File(d,"data.txt");
+        FileReader reader=new FileReader(file);
+        int ch;
+        StringBuilder builder = new StringBuilder();
+        while((ch=reader.read())!=-1)
+        {
+            builder.append ((char)ch);
+
+        }
+        Toast.makeText(this,"Data:" +builder.toString(),Toast.LENGTH_SHORT).show();
     }
 }
