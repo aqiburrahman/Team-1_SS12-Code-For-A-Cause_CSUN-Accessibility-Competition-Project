f65e600e4ce7dd12a51f661ed1700c592c28a10f
abi755 <abigail.rosas.755@my.csun.edu>
2022-01-22 18:29:52 -0800

    test pt 2

--- app/src/main/java/com/example/team1/MainActivity.java
+++ app/src/main/java/com/example/team1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.team1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void disable(View V){
+        View myView = findViewById(R.id.imageButton6);
+        myView.setEnabled(false);
+        myView.setVisibility(View.GONE);
+
+        //Button button = (Button) myView;
+        //button.setText("1");
+
+        View myView2 = findViewById(R.id.imageButton7);
+        myView2.setEnabled(false);
+
+        //Button button2 = (Button) myView2;
+        //button2.setText("2");
+
+        /*
+        V.setEnabled(false);
+        Button button =(Button) V;
+        button.setText("Disabled");
+        */
     }
 }
