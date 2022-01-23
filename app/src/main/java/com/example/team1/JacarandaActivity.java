1c8e34e749b003bff292efae2594ca8cb321f3e8
vanessa5r7 <vanessa.juarez.448@my.csun.edu>
2022-01-22 18:21:50 -0800

    fixed map layouts

---
+++ app/src/main/java/com/example/team1/JacarandaActivity.java
@@ -0,0 +1,15 @@
+package com.example.team1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.content.Intent;
+import android.os.Bundle;
+
+public class JacarandaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jacaranda);
+    }
+}
--- app/src/main/java/com/example/team1/MainActivity.java
+++ app/src/main/java/com/example/team1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.team1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void jacaranda(View v) {
+        Intent i = new Intent(this, JacarandaActivity.class);
+        startActivity(i);
+    }
+    public void education(View v) {
+        Intent i = new Intent(this, EducationActivity.class);
+        startActivity(i);
+    }
+    public void sierra(View v) {
+        Intent i = new Intent(this, SierraActivity.class);
+        startActivity(i);
     }
 }
