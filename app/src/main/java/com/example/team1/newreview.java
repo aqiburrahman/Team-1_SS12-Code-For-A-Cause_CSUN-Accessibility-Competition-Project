d1011c86786b5492d8f028ffa012ce9e0d0c079d
abi755 <abigail.rosas.755@my.csun.edu>
2022-01-23 00:58:57 -0800

    test pt 3

--- app/src/main/java/com/example/team1/newreview.java
+++ app/src/main/java/com/example/team1/newreview.java
@@ -2,6 +2,8 @@ package com.example.team1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,5 +20,21 @@ public class newreview extends AppCompatActivity {
         TextView t = findViewById(R.id.source);
         String input = t.getText().toString();
         ((TextView)findViewById(R.id.output)).setText(input);
+    }
+    public void disableGreen(View v){
+        View ImageB = findViewById(R.id.imageButton);
+        ImageB.setEnabled(false);
+        ImageB.setClickable(false);
+        ImageB.setBackgroundColor(Color.parseColor("#008000"));
+    }
+    public void disableRed(View v){
+        View ImageB = findViewById(R.id.imageButton2);
+        ImageB.setEnabled(false);
+        ImageB.setClickable(false);
+        ImageB.setBackgroundColor(Color.parseColor("#8B0000"));
+    }
+    public void uploaderClick(View v) {
+        Intent i = new Intent(this, uploader.class);
+        startActivity(i);
     }
 }
